a1a7534a6a77728e8a8ab2e96f7fc0f3c49c9268
Perilix <166854906+Perilix@users.noreply.github.com>
2026-08-31 15:18:16 +0200

    feat(archi) : create web project

---
+++ apps/web/src/app/app.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed } from '@angular/core/testing';
+import { App } from './app';
+
+describe('App', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [App],
+    }).compileComponents();
+  });
+
+  it("crée l'application", () => {
+    const fixture = TestBed.createComponent(App);
+    expect(fixture.componentInstance).toBeTruthy();
+  });
+});
